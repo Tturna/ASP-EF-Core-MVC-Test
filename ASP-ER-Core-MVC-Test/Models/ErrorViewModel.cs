fdb1f5058a1fd3246a2a8c32b6e9a0fb68ab6142
Tturna <42873350+Tturna@users.noreply.github.com>
2024-03-21 11:04:35 +0200

    init

---
+++ ASP-ER-Core-MVC-Test/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASP_ER_Core_MVC_Test.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
